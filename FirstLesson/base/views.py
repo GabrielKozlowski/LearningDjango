827ac1480d93ed23ae27237488319bc1c3a11d0b
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-11-02 19:19:04 +0100

    amodified base

--- FirstLesson/base/views.py
+++ FirstLesson/base/views.py
@@ -1,11 +1,24 @@
+from multiprocessing import context
 from urllib import request
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
+rooms = [
+    {'id' :1, 'name' :'Lets lear python!'},
+    {'id' :2, 'name' :'Design with me'},
+    {'id' :3, 'name' :'Frontend developer'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
