be4d36b2a176860b71ccae64fa239055366cf74c
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-11-08 16:07:18 +0100

    added css.style , js

--- FirstLesson/base/forms.py
+++ FirstLesson/base/forms.py
@@ -1,3 +1,4 @@
+from csv import excel
 from django.forms import ModelForm
 from .models import Room
 
@@ -6,3 +7,4 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host','participants']
